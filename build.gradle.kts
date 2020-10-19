b39b56ad4741d902a06780bb2c194034b12cf9ec
Alexander Nozik <altavir@gmail.com>
2020-10-19 11:33:25 +0300

    Fix dependencies notation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins{
+plugins {
     kotlin("jvm") version "1.4.10"
     application
 }
@@ -25,11 +25,9 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
     implementation("org.apache.commons:commons-math3:3.6.1")
-    compile("commons-cli","commons-cli",  "1.4")
-    compile("kscience.plotlykt","plotlykt-server", "0.2.0")
-    testImplementation(
-        "junit:junit:4.12"
-    )
+    implementation("commons-cli", "commons-cli", "1.4")
+    implementation("kscience.plotlykt", "plotlykt-server", "0.2.0")
+    testImplementation("junit:junit:4.12")
 }
 
 
